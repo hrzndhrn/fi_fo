311c8c49695113b0c25c0f156f89aad249f196cd
Marcus Kruse <esurk.sucram@gmail.com>
2019-10-03 20:44:42 +0200

    Set project.elixir to 1.7

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule FiFo.MixProject do
     [
       app: :fi_fo,
       version: "0.1.0",
-      elixir: "~> 1.9",
+      elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       description: "Just a FIFO queue.",
